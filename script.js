e7b85718135fd0f2808cb64c0f9330e66dcbbe09
zinconer-glitch <zinconer@gmail.com>
2025-10-28 11:26:54 +0100

    site terminé

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu-icon = document.getElementById('menu-icon')
+const menu = document.getElementById('menu')
+
+menu-icon .addEvenlistener('click' , () => {
+    nav.style.display = 'block'
+});
